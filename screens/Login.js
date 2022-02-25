26d04ae9624659eaffdbc1017c6e2a138e1deb1f
jpsystem <jpsystem@gmail.com>
2022-02-24 22:28:17 -0300

    Commit inical do projeto

---
+++ screens/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { StyleSheet,  View } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import Icon  from 'react-native-vector-icons/FontAwesome';
+
+export default function Login({navigation}) {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const entrar = () => {
+    navigation.reset({
+        index: 0,
+        routes: [{name: "Principal"}]
+    })
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text h3>Entre no TemTudaki</Text>
+      <Input
+        placeholder="E-mail"
+        leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+        onChangeText={value => setEmail(value)}
+        keyboardType='email-address'
+      />
+      <Input
+        placeholder="Sua senha"
+        leftIcon={{ type: 'font-awesome', name: 'lock' }}
+        onChangeText={value => setPassword(value)}
+        secureTextEntry={true}
+      />
+      <Button
+        icon={
+          <Icon
+            name="check"
+            size={15}
+            color="white"
+          />
+        }
+        title="Entrar"
+        onPress={() => entrar()}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
